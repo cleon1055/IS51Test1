475c979ffb5062ebe5e39563beb28f99bd256d73
cleon1055 <cleon1055@gmail.com>
2020-11-12 16:52:25 -0800

    structured-english

---
+++ test_1.py
@@ -0,0 +1,18 @@
+
+"""
+
+This program will determine which payment option, out of two, is better in terms of higher pay.
+The first option pays 100 dollars a day for 10 days. The second option pays 1 dollar on the first
+day with the amount paid then doubling each day after that for 10 days. There will be two fuctions,
+one for each pay option.
+
+Function 1 will calculate the pay total for option 1, and fuction 2 will calculate the pay for option 2.
+Function 1 will output 100 * 10.
+Function 2 will loop 10 times, each time doubling the previous amount and adding it to the total. 
+
+If amounts for option 1 and option 2 are equal, program will display "Option 1 and Option 2
+pay the same."
+If option 1 pays more, program will output to user "Option 1 is better."
+If option 2 pays more, program will output to user "option 2 is better." 
+
+"""

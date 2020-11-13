b3c53238c8e1bcd24c1cd1877c508a7dd7672463
cleon1055 <cleon1055@gmail.com>
2020-11-12 17:06:00 -0800

    pseudo-code

--- test_1.py
+++ test_1.py
@@ -16,3 +16,30 @@ If option 1 pays more, program will output to user "Option 1 is better."
 If option 2 pays more, program will output to user "option 2 is better." 
 
 """
+
+"""
+#option1
+ return 100 * 10
+
+#option2
+ amount = 1
+ list1 = []
+ loop 10 times
+   add amount to list1
+   amount *= 2
+ sum = sum of all items in loop
+ return sum
+
+#main
+ var1 = option1
+ var2 = option2
+
+ if var1 = var 2
+   "Option 1 and Option 2 pay the same" 
+ if var1 > var2
+   "Option 1 is better."
+ else
+   "Option 2 is better."  
+
+Main
+""" 
